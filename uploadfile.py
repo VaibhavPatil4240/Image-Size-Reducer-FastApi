23ac85cd61d324db552c5b1e55b29e63a2d757c7
VP <vaibhavpatil4240@gmail.com>
2022-08-20 23:09:31 +0530

    Updated upload.py

--- uploadfile.py
+++ uploadfile.py
@@ -1,18 +1,14 @@
 import requests
 import os
-url = 'http://127.0.0.1:8000/upload'
-path=input("Path of image or directory containing multiple images")
 
-if(os.path.isdir(path)):
-    for file_name in os.listdir(path):
-        file = {'file': open(path+"/"+file_name, 'rb')}
-        resp = requests.post(url=url, files=file)
-        filename=resp.headers.get("Content-Disposition").split("filename=")[1].replace('"',"")
-        resp.raise_for_status()
-        with open(filename, 'wb') as f:
-            for chunk in resp.iter_content(): 
-                f.write(chunk)
-else:
+image_extensions=('png','PNG','jpg','jpeg','JPG','JPEG')
+url = 'http://127.0.0.1:8000/upload'
+path=input("Enter path to image: ")
+if(not os.path.isfile(path)):
+    print("Specified path is not a file...")
+    exit(0)
+extention=path.split('.')[-1]
+if(extention in image_extensions):
     file = {'file': open(path, 'rb')}
     resp = requests.post(url=url, files=file)
     filename=resp.headers.get("Content-Disposition").split("filename=")[1].replace('"',"")
@@ -20,3 +16,5 @@ else:
     with open(filename, 'wb') as f:
         for chunk in resp.iter_content(): 
             f.write(chunk)
+else:
+    print("Unsupported file...\nSupported files: png, jpg, jpeg")
