79c428383be140e56b47028ceafba8d492dc56c3
VP <vaibhavpatil4240@gmail.com>
2022-08-20 20:43:23 +0530

    Added path check

--- uploadfile.py
+++ uploadfile.py
@@ -2,9 +2,10 @@ import requests
 import os
 url = 'http://127.0.0.1:8000/upload'
 path=input("Path of image or directory containing multiple images")
+
 if(os.path.isdir(path)):
     for file_name in os.listdir(path):
-        file = {'file': open(path+file_name, 'rb')}
+        file = {'file': open(path+"/"+file_name, 'rb')}
         resp = requests.post(url=url, files=file)
         filename=resp.headers.get("Content-Disposition").split("filename=")[1].replace('"',"")
         resp.raise_for_status()
